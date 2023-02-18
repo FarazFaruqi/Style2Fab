12235a3fb0560a91b52a15b0052584ad13414bab
Ahmed Katary <akatary@dhcp-10-29-76-143.dyn.MIT.EDU>
2023-02-17 20:01:14 -0500

    updating UI

--- plugin/__init__.py
+++ plugin/__init__.py
@@ -78,7 +78,7 @@ props = {
 
     'mesh_dir': StringProperty(
         name = "", 
-        default = "/home/ubuntu/fa3ds/backend/segment/segment_utils/models"
+        default = "/home/ubuntu/fa3ds/backend/segment/segment_utils/ui_models"
     ),
 
     'models': CollectionProperty(
