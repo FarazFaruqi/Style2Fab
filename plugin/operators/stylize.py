b935d12a83bb47bfda886c523613fd711d11863d
Tarik <ht.tarikhasic@gmail.com>
2023-10-27 18:26:29 -0400

    Prepping for presentation (fixed only one POST for cat)

--- plugin/operators/stylize.py
+++ plugin/operators/stylize.py
@@ -44,58 +44,84 @@ class Stylize_OT_Op(bpy.types.Operator):
 
         url = f"{domain}/stylize/"
         self.report({'INFO'}, f"Stylizing {len(objs)} meshes with {len(faces)} faces and {len(vertices)} vertices ...")
+
+        if context.scene.i == 0:
         
-        data = json.dumps({'vertices': vertices, 'faces': faces, 'prompt': prompt, 'selection': selection, 'remesh': False, "index": context.scene.i, "model": "r"})
-        
-        try:
-            response = requests.post(url = url, json = data).json()
-            faces = response['faces']
-            vertices = response['vertices']
-            materials = response['materials']
-            self.report({'INFO'}, f"Stylized mesh successfully!")
+            data = json.dumps({'vertices': vertices, 'faces': faces, 'prompt': prompt, 'selection': selection, 'remesh': False, "index": context.scene.i, "model": "r"})
             
-            mesh_name = f"{obj.name}_stylized"
-            new_mesh = bpy.data.meshes.new(mesh_name)
-            new_mesh.from_pydata(vertices, [], faces)
-  
-            new_mesh.update()
-            new_object = bpy.data.objects.new(mesh_name, new_mesh)
-            bpy.context.scene.collection.objects.link(new_object)
-            bpy.context.view_layer.objects.active = new_object
+            try:
+                response = requests.post(url = url, json = data).json()
+                faces = response['faces']
+                vertices = response['vertices']
+                materials = response['materials']
+                self.report({'INFO'}, f"Stylized mesh successfully!")
+                
+                mesh_name = f"{obj.name}_stylized"
+                new_mesh = bpy.data.meshes.new(mesh_name)
+                new_mesh.from_pydata(vertices, [], faces)
+    
+                new_mesh.update()
+                new_object = bpy.data.objects.new(mesh_name, new_mesh)
+                bpy.context.scene.collection.objects.link(new_object)
+                bpy.context.view_layer.objects.active = new_object
 
-            self.report({'INFO'}, f"Added new mesh {mesh_name} ...")
-                          
-            _assign_materials(new_object, materials)
+                self.report({'INFO'}, f"Added new mesh {mesh_name} ...")
+                            
+                _assign_materials(new_object, materials)
 
-            bpy.ops.object.mode_set(mode='OBJECT')
-        except Exception as error: self.report({'WARNING'}, f"Error occured while stylizing mesh\n{report(traceback.format_exc())}")
-        
-        data = json.dumps({'vertices': vertices, 'faces': faces, 'prompt': prompt, 'selection': selection, 'remesh': False, "index": context.scene.i, "model": "p"})
-        
-        try:
-            response = requests.post(url = url, json = data).json()
-            faces = response['faces']
-            vertices = response['vertices']
-            materials = response['materials']
-            self.report({'INFO'}, f"Stylized mesh successfully!")
+                bpy.ops.object.mode_set(mode='OBJECT')
+            except Exception as error: self.report({'WARNING'}, f"Error occured while stylizing mesh\n{report(traceback.format_exc())}")
             
-            mesh_name = f"{obj.name}_stylized"
-            new_mesh = bpy.data.meshes.new(mesh_name)
-            new_mesh.from_pydata(vertices, [], faces)
-  
-            new_mesh.update()
-            new_object = bpy.data.objects.new(mesh_name, new_mesh)
-            bpy.context.scene.collection.objects.link(new_object)
-            bpy.context.view_layer.objects.active = new_object
+            data = json.dumps({'vertices': vertices, 'faces': faces, 'prompt': prompt, 'selection': selection, 'remesh': False, "index": context.scene.i, "model": "p"})
+            
+            try:
+                response = requests.post(url = url, json = data).json()
+                faces = response['faces']
+                vertices = response['vertices']
+                materials = response['materials']
+                self.report({'INFO'}, f"Stylized mesh successfully!")
+                
+                mesh_name = f"{obj.name}_stylized"
+                new_mesh = bpy.data.meshes.new(mesh_name)
+                new_mesh.from_pydata(vertices, [], faces)
+    
+                new_mesh.update()
+                new_object = bpy.data.objects.new(mesh_name, new_mesh)
+                bpy.context.scene.collection.objects.link(new_object)
+                bpy.context.view_layer.objects.active = new_object
 
-            self.report({'INFO'}, f"Added new mesh {mesh_name} ...")
-                          
-            _assign_materials(new_object, materials)
+                self.report({'INFO'}, f"Added new mesh {mesh_name} ...")
+                            
+                _assign_materials(new_object, materials)
 
-            bpy.ops.object.mode_set(mode='OBJECT')
+                bpy.ops.object.mode_set(mode='OBJECT')
 
-        except Exception as error: self.report({'WARNING'}, f"Error occured while stylizing mesh\n{report(traceback.format_exc())}")
-        
+            except Exception as error: self.report({'WARNING'}, f"Error occured while stylizing mesh\n{report(traceback.format_exc())}")
+        elif context.scene.i == 1:
+            data = json.dumps({'vertices': vertices, 'faces': faces, 'prompt': prompt, 'selection': selection, 'remesh': False, "index": context.scene.i, "model": "p"})
+            try:
+                response = requests.post(url = url, json = data).json()
+                faces = response['faces']
+                vertices = response['vertices']
+                materials = response['materials']
+                self.report({'INFO'}, f"Stylized mesh successfully!")
+                
+                mesh_name = f"{obj.name}_stylized"
+                new_mesh = bpy.data.meshes.new(mesh_name)
+                new_mesh.from_pydata(vertices, [], faces)
+    
+                new_mesh.update()
+                new_object = bpy.data.objects.new(mesh_name, new_mesh)
+                bpy.context.scene.collection.objects.link(new_object)
+                bpy.context.view_layer.objects.active = new_object
+
+                self.report({'INFO'}, f"Added new mesh {mesh_name} ...")
+                            
+                _assign_materials(new_object, materials)
+
+                bpy.ops.object.mode_set(mode='OBJECT')
+
+            except Exception as error: self.report({'WARNING'}, f"Error occured while stylizing mesh\n{report(traceback.format_exc())}")
         return {'FINISHED'}
 
 ### Helper Functions ###
